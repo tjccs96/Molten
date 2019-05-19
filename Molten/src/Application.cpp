1eca7c76f4f0454d5298ac5f0a4dddb84130d1f7
Tjccs <tiago.jccs@protonmail.com>
2019-05-20 00:29:24 +0100

    Aditional code

--- Molten/src/Application.cpp
+++ Molten/src/Application.cpp
@@ -6,14 +6,12 @@ int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
+	/* Initialize the library: GLFW */
 	if (!glfwInit())
 		return -1;
 
-	
-
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "OpenGL Renderer", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -23,18 +21,26 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK)
-		std::cout << "Err";
-
+		std::cout << glewGetErrorString(glewInit());
+	
+	// Define positions for the triangle to be drawn.
 	float vertices[6] = {
 		-0.5f, -0.5f,
 		 0.0f,  0.0f,
 		 0.5f,  0.5f
 	};
 
+	// Create buffer, bind the buffer and get data into it.
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
